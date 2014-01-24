540f51cfe634873fc828bb13281dd2c6f81b6161
Dzakub <dzakub@gmail.com>
2014-01-24 22:14:15 +0100

    Test entity

---
+++ entities/entities/metal_small/init.lua
@@ -0,0 +1,48 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+	self:SetModel("models/props_junk/garbage_takeoutcarton001a.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	local phys = self:GetPhysicsObject()
+
+	phys:Wake()
+
+	self.damage = 10
+end
+
+function ENT:OnTakeDamage(dmg)
+	self.damage = self.damage - dmg:GetDamage()
+
+	if (self.damage <= 0) then
+		local effectdata = EffectData()
+		effectdata:SetOrigin(self:GetPos())
+		effectdata:SetMagnitude(2)
+		effectdata:SetScale(2)
+		effectdata:SetRadius(3)
+		util.Effect("Sparks", effectdata)
+		self:Remove()
+	end
+end
+
+function ENT:Use(activator,caller)
+	--caller:setSelfDarkRPVar("Energy", math.Clamp((caller:getDarkRPVar("Energy") or 0) + 100, 0, 100))
+	--umsg.Start("AteFoodIcon", caller)
+	--umsg.End()
+
+	local effectdata = EffectData()
+	effectdata:SetOrigin(self:GetPos())
+	effectdata:SetMagnitude(2)
+	effectdata:SetScale(2)
+	effectdata:SetRadius(3)
+	util.Effect("Sparks", effectdata)
+	self:Remove()
+end
+
+function ENT:OnRemove()
+
+end
